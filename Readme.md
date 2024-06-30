87bb358ef82bad924ee277fdd002a64c88e62b54
Aashish1-1-1 <aashishadhikari693@gmail.com>
2024-06-30 13:40:25 +0545

    [Added: Round Robin]

--- Readme.md
+++ Readme.md
@@ -1,5 +1,11 @@
 ## LoadBalancer
-Implementing a minimal loadbalancer using Esp8236.
+A minimal wannabe loadbalancer using Esp8236.
 
 ## Architecture
 ![archi](https://github.com/Aashish1-1-1/LoadBalancer/blob/main/images/archi.svg)
+
+## Note
+This is not how the actual loadbalancer works in actual loadbalancer are responsible for a tunnel between server and client here it is only redirecting to server and rest is handeled by server.
+
+## To Run
+Clone the repo in srf file input wifi ssid and password and upload in ESP8266 and visit the ip printed in serial monitor
--- src/src.ino
+++ src/src.ino
@@ -1,7 +1,40 @@
-void setup(){
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
+//Network 
+const char* ssid     = "135966@ClassicTech";
+const char* password = "12345678";
+//Servers
+const char* servers[] = {"https://www.facebook.com/","https://www.instagram.com","https://www.twitter.com","https://www.youtube.com"};
+ int count=0;
+//port
+ESP8266WebServer server(80);
+void setup(){
+	Serial.begin(115200);
+	Serial.print("Connecting to ");
+  	Serial.println(ssid);
+  	WiFi.begin(ssid, password);
+  	while (WiFi.status() != WL_CONNECTED) {
+  	  delay(500);
+  	  Serial.print(".");
+  	}
+  	Serial.println("");
+  	Serial.println("WiFi connected.");
+  	Serial.println("IP address: ");
+  	Serial.println(WiFi.localIP());
+		server.on("/",handleRedirection);
+  	server.begin();
+}
+const char* RoundRobinAlgo(){
+  int temp=count;
+	count=(count+1)%4;
+	return servers[temp];		
+}
+void handleRedirection(){
+	server.sendHeader("Location",RoundRobinAlgo(),true);  // true for 302 redirect
+  	server.send(302, "text/plain", "Redirecting...");
 }
 
 void loop(){
-
+	server.handleClient();
 }
